fd5cac6403921bdbf44a6aa0bb1974273ea579e0
ANIKET RITAM <140789442+Aashwin11@users.noreply.github.com>
2025-06-23 14:35:58 +0000

    Added seperaete backend.tf

--- main.tf
+++ main.tf
@@ -93,11 +93,3 @@ module "alb" {
   env-name = terraform.workspace
 }
 
-terraform {
-  backend "s3" {
-    bucket="bucket-terraform-github-actions-project"
-    key="envs/${terraform.workspace}/terraform.tfstate"
-    region=us-east-1
-    dynamodb_table = "terrraform-lock"
-  }
-}
